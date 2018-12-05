726df9d950910ad5ff951181e5f51a9940658c17
himamis <megette_a@yahoo.com>
2018-12-05 16:42:25 +0200

    Update

--- trainer/tf_train_inkml.py
+++ trainer/tf_train_inkml.py
@@ -69,10 +69,10 @@ logging.debug("Image2Latex: Start create model: {}".format(str(datetime.now().ti
 device = '/cpu:0' if use_gpu == 'n' else '/gpu:{}'.format(use_gpu)
 with tf.device(device):
     model = tf_model.Model(len(encoding_vb),
-                           filter_sizes=[32, 64, 128, 256, 512],
-                           encoder_size=256,
-                           decoder_units=512,
-                           attention_dim=512,
+                           filter_sizes=[32, 64],
+                           encoder_size=16,
+                           decoder_units=32,
+                           attention_dim=32,
                            bidirectional=True,
                            conv_kernel_init=tf.contrib.layers.xavier_initializer(),
                            conv_bias_init=tf.initializers.constant(0.01),
