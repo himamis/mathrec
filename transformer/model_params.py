b35721546880f08fe9f05d172755f40977649d5b
himamis <megette_a@yahoo.com>
2019-02-07 13:33:32 +0200

    Increase warmup steps, and beam_size

--- transformer/model_params.py
+++ transformer/model_params.py
@@ -99,13 +99,12 @@ CUSTOM_PARAMS = BASE_PARAMS.copy()
 CUSTOM_PARAMS.update(
     hidden_size=512,
     num_heads=8,
-    learning_rate_warmup_steps=5000,
+    learning_rate_warmup_steps=12000,
     learning_rate=2.0,
     layer_postprocess_dropout=0.2,
     attention_dropout=0.2,
     relu_dropout=0.2,
-    # learning_rate=0.005,
 
-    beam_size=2,
+    beam_size=4,
     alpha=0.5
 )
