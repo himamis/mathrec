9962536f5641a1b8e30f2f219749cccad67bfde3
himamis <megette_a@yahoo.com>
2019-03-08 15:23:43 +0200

    Update

--- object_detection/train_classification.py
+++ object_detection/train_classification.py
@@ -1,59 +1,106 @@
 import os
 import tensorflow as tf
-#import slim.nets.resnet_v2 as resnet
+import slim.nets.resnet_v2 as resnet
 import slim.nets.vgg as vgg
 from object_detection.generator import create_generator, create_image_labels
+import trainer.params as par
 
 
-import trainer.params as params
-
-validation_every_n_step = 10
-decay = 1e-4
-batch_size = 32
-
-
-def create_input_fn(training=True):
-    generator, obj = create_generator(os.path.join(params.data_base_dir, "training.pkl" if training else "validation.pkl"))
-    return lambda: create_image_labels(generator, batch_size=batch_size, repeat=None if training else obj.size()), obj
+def create_input_fn(training=True, batch_size=32):
+    generator, obj = create_generator(os.path.join(par.data_base_dir, "training.pkl" if training else "validation.pkl"))
+    return create_image_labels(generator, batch_size=batch_size, repeat=None if training else obj.size())
 
 
 def model_fn(features, labels, mode, params):
-    logits, _ = vgg.vgg_16(
+    if type == "vgg16":
+        logits, _ = vgg.vgg_16(
+                tf.to_float(features),
+                num_classes=101,
+                dropout_keep_prob=1 - params.dropout_rate,
+                is_training=mode == tf.estimator.ModeKeys.TRAIN)
+    elif type == "resnet50":
+        logits, _ = resnet.resnet_v2_50(
             tf.to_float(features),
             num_classes=101,
-            dropout_keep_prob=0.4,
-            is_training=mode == tf.estimator.ModeKeys.TRAIN)
+            is_training=mode == tf.estimator.ModeKeys.TRAIN
+        )
+    else:
+        assert "Type must be available"
+
+    class_ids = tf.argmax(logits, 1)
+    accuracy = tf.metrics.accuracy(labels=labels, predictions=class_ids, name='acc_op')
+    mean_per_class = tf.metrics.mean_per_class_accuracy(labels=labels, predictions=class_ids, num_classes=101)
+
+    metrics = {
+        'accuracy': accuracy,
+        'mean_per_class_accuracy': mean_per_class
+    }
+    tf.summary.scalar('accuracy', accuracy[1])
+    tf.summary.scalar('mean_per_class_accuracy', mean_per_class[1])
+
     labels = labels - 1
     labels_one_hot = tf.one_hot(labels, 101, dtype=tf.int32)
-    # loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels_one_hot, logits=logits)
-    loss = tf.losses.softmax_cross_entropy(onehot_labels=labels_one_hot, logits=logits)
-    accuracy = tf.metrics.accuracy(labels=labels, predictions=tf.argmax(logits, 1), name='acc_op')
-    metrics = {'accuracy': accuracy}
-    tf.summary.scalar('accuracy', accuracy[1])
+
+    if mode == tf.estimator.ModeKeys.TRAIN or mode == tf.estimator.ModeKeys.EVAL:
+        loss = tf.losses.softmax_cross_entropy(onehot_labels=labels_one_hot, logits=logits)
+    else:
+        loss = None
 
     if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.AdamOptimizer(0.001)
+        optimizer = tf.train.AdamOptimizer(params.learning_rate)
         train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
-    elif mode == tf.estimator.ModeKeys.EVAL:
-        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
-    else: # tf.estimator.ModeKeys.EVAL
+    else:
+        train_op = None
+    if mode == tf.estimator.ModeKeys.PREDICT:
         predictions = {
             'class_ids': tf.argmax(logits),
             'probabilities': tf.nn.softmax(logits),
             'logits': logits
         }
-        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+    else:
+        predictions = None
+
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        predictions=predictions,
+        loss=loss,
+        train_op=train_op,
+        eval_metric_ops=metrics)
+
+
+def create_estimator(run_config, hparams):
+    return tf.estimator.Estimator(
+        model_fn=model_fn,
+        params=hparams,
+        config=run_config
+    )
+
+
+def create_train_and_eval_spec(hparams):
+    train_spec = tf.estimator.TrainSpec(
+        input_fn=lambda: create_input_fn(training=True),
+        max_steps=hparams.max)
+    eval_spec = tf.estimator.EvalSpec(
+        input_fn=create_input_fn(training=False),
+        steps=None)
+    return train_spec, eval_spec
 
 
 def main():
-    classifier = tf.estimator.Estimator(
-        model_fn=model_fn,
-        model_dir=os.path.join(params.tensorboard_log_dir, params.tensorboard_name)
+    hparams = tf.contrib.training.HParams(
+        type='resnet50',
+        max_steps=50000,
+        num_epochs=100,
+        batch_size=32,
+        learning_rate=0.001,
+        dropout_rate=0.6
+    )
+    run_config = tf.estimator.RunConfig(
+        log_step_count_steps=200,
+        tf_random_seed=1234567,
+        model_dir=os.path.join(par.tensorboard_log_dir, par.tensorboard_name)
     )
 
-    train_input_fn, _ = create_input_fn(training=True)
-    eval_input_fn, obj = create_input_fn(training=False)
-    train_spec = tf.estimator.TrainSpec(train_input_fn, max_steps=50000)
-    eval_spec = tf.estimator.EvalSpec(eval_input_fn, steps=None)
-    tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
+    estimator = create_estimator(run_config, hparams)
+    train_spec, eval_spec = create_train_and_eval_spec(hparams)
+    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
