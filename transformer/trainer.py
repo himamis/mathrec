c86850f06c32deedbfce0c474811780a21af392b
himamis <megette_a@yahoo.com>
2019-05-18 10:57:19 +0300

    Update

--- transformer/trainer.py
+++ transformer/trainer.py
@@ -47,7 +47,7 @@ def create_generators(batch_size=32):
         sorted_data = sorted(validating, key=lambda a: len(a[1]))
         total_length = len(sorted_data)
         no_bins = 5
-        bins = [] * no_bins
+        bins = [[]] * no_bins
         data_per_bin = int(total_length / no_bins)
         bin_max_size = [i * data_per_bin for i in range(no_bins)]
 
